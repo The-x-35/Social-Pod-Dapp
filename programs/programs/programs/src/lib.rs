ce0a0f3cb1fb8684dfbf0597d72f66fc45081049
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-09-14 19:47:46 +0530

    Defined structs

--- programs/programs/programs/src/lib.rs
+++ programs/programs/programs/src/lib.rs
@@ -3,6 +3,10 @@ use anchor_spl::token::{self, Token};
 
 declare_id!("8MKSwpR9qs9zZj5bNUpzY5s8zijogrpB1PXDsBbziDVG");
 
+const TEXT_LENGTH: usize = 1024;
+const USER_NAME_LENGTH: usize = 100;
+const USER_URL_LENGTH: usize = 255;
+
 #[program]
 pub mod programs {
     use super::*;
@@ -42,6 +46,34 @@ pub struct StateAccount {
     pub post_count: u64,
 }
 
+#[derive(Accounts)]
+pub struct CreatePost<'info> {
+
+    #[account(mut, seeds = [b"state".as_ref()], bump)]
+    pub state: Account<'info, StateAccount>,
+
+
+    #[account(
+        init,
+        seeds = [b"post".as_ref(), state.post_count.to_be_bytes().as_ref()],
+        bump,
+        payer = authority,
+        space = size_of::<PostAccount>() + TEXT_LENGTH + USER_NAME_LENGTH + USER_URL_LENGTH
+    )]
+    pub post: Account<'info, PostAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: UncheckedAccount<'info>,
+
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    pub clock: Sysvar<'info, Clock>,
+}
+
+#[account]
 pub struct PostAccount {
     pub authority: Pubkey,
     pub text: String,
